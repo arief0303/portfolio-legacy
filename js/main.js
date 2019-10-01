f1f499561e0d11932c8b5d53b883ee84e8ddeecf
Arief <arief0303@gmail.com>
2019-10-01 23:38:56 +0200

    title anim implementation

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+// When the user scrolls down 50px from the top of the document, hide title
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop >= 10 || document.documentElement.scrollTop >= 10) {
+    document.getElementById("title").style.fontSize = "0px";
+    document.getElementById("title").style.margin = "0%";
+    document.body.scrollTop = 11;
+    document.documentElement.scrollTop = 11;
+  } else {
+    document.getElementById("title").style.fontSize = "120px";
+    document.getElementById("title").style.margin = "25%";
+  }
+}
