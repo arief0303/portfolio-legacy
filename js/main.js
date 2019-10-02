29ce326882c2351701916223117ffde00e323c4c
Arief <arief0303@gmail.com>
2019-10-02 14:31:49 +0200

    // WIP

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+/*window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    document.getElementById("title").style.fontSize = "0px";
+    document.getElementById("title").style.margin = "50%";
+    
+  } else {
+    document.getElementById("title").style.fontSize = "90px";
+  }
+}*/
