5edac6224f32907cfd4827e34c0d31649ad83e65
Arief <arief0303@gmail.com>
2019-10-07 15:09:55 +0200

    checkpoint

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-// OVERLAY SCRIPT___________________________________________________________________
-/* Open when someone clicks on the span element */
+//OFF-CANVAS MENU_______________________________________________________________________
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-  document.getElementById("navButton") = "100%";
-
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0%";
 }
+
+//hamburger menu
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  if(hamburger.className == "hamburger hamburger--collapse is-active"){openNav();}
+  else{closeNav();}
+});
