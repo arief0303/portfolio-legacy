c6ec40a1369d2f2026489f24603265cc388eccae
Arief <arief0303@gmail.com>
2020-04-06 14:36:56 +0200

    local https implementation and minor threejs tweaks

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,40 @@
-self.addEventListener("install",(installing)=>{
-  console.log("Service Worker: I am being installed, hello world!");
+var pwaCache = 'cache-v0.1';
+var resourceToPrecache = [
+  '/',
+  'index.html',
+  'styles/style.css',
+  'manifest.json'
+]
+
+self.addEventListener("activate", (activating) => {
+  console.log("Service Worker: Activation succeed");
 });
 
-self.addEventListener("activate",(activating)=>{	
-  console.log("Service Worker: All systems online, ready to go!");
+self.addEventListener("fetch", (fetching) => {
+  console.log("Service Worker: Fetch intercepted");
 });
 
-self.addEventListener("fetch",(fetching)=>{   
-  console.log("Service Worker: User threw a ball, I need to fetch it!");
+self.addEventListener("push", (pushing) => {
+  console.log("Service Worker: I received some push data, but because I am still very simple I don't know what to do with it :(");
 });
 
-self.addEventListener("push",(pushing)=>{
-	console.log("Service Worker: I received some push data, but because I am still very simple I don't know what to do with it :(");
-})
+self.addEventListener("install", (installing) => {
+  console.log("Service Worker: Installation in progress");
+});
+
+/*pwaCache.respondWith(
+  caches.match(pwaCache.request.url).then((response) => {
+    console.log("Service Worker: Fetching resource " + pwaCache.request.url);
+    return response || fetch(pwaCache.request).then((response) => {
+      console.log("Service Worker: Resource " + pwaCache.request.url + " not available in cache");
+      return caches.open(pwaCache).then((cache) => {
+        console.log("Service Worker: Caching (new) resource " + pwaCache.request.url);
+        cache.put(fetching.request, response.clone());
+        return response;
+      });
+    }).catch(function () {
+      console.log("Service Worker: Fetching online failed, HAALLPPPP!!!");
+      //Do something else with the request (respond with a different cached file)
+    })
+  })
+);*/
